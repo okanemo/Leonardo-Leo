56e78e6fc8831a77c6f344513e901fe0c5b88435
Leonardo81 <leonardoeinstein2000@gmail.com>
2021-03-21 21:55:54 +0700

    add migration file

---
+++ database/migrations/2021_03_21_142403_sub_category.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SubCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_category', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            // $table->unsignedBigInteger('category_id');
+            $table->foreignId('category_id')->constrained('category');
+            $table->integer('amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_category');
+    }
+}
